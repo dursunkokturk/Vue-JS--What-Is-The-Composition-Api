3c32433cd7df60756e7e23f080b96af49ac46a6f
Dursun Köktürk <dursunserefkokturk@gmail.com>
2024-08-09 15:56:11 +0300

    operate-inside-an-object-and-inside-an-html-object-using-the-ref-attribute

--- Lecture-119-Template-Refs/src/App.vue
+++ Lecture-119-Template-Refs/src/App.vue
@@ -2,9 +2,6 @@
     <h1>
         Ad: {{ firstName }}
     </h1>
-    
-    <!-- ref Attribute Kullanilarak 
-          Objenin Icindeki Attribute lara Ulasiyoruz -->
     <h2 ref="myRef">
         Yaş {{ age }}
     </h2>
@@ -14,35 +11,21 @@
 </template>
 
 <script>
-  import {ref} from 'vue'
+    import {ref} from 'vue'
 
-  export default {
-      name: 'App',
-      setup(){
-          let firstName ='Dursun';
-          let age=32;
+    export default {
+        name: 'App',
+        setup(){
+            let firstName ='Dursun';
+            let age=32;
 
-          /* ref Attribute unu 
-              myRef Degiskeni Uzerinden Yonetiyoruz */
-          const myRef= ref(null)
-          const handleClick = () =>{
-
-              /* myref.value Attribute Kullanilarak 
-                  Obje Icindeki HTML Elementine Ulasiyoruz */
-              console.log(myRef,myRef.value)
-
-              /* myref.value Attribute Kullanilarak 
-                  ref Attribute Bulunan HTML Elementinde
-                  Obje Icindeki HTML Elementine Class Attribute Ekliyoruz
-                  Ekledigimiz Class Attribute una Deger Veriyoruz */
-              myRef.value.classList.add('Dursun');
-
-              /* myref.value Attribute Kullanilarak 
-                  ref Attribute Bulunan HTML Elementinde
-                  Obje Icindeki HTML Elementine Yazi Yazdiriyoruz */
-              myRef.value.textContent='Dursun Köktürk'
-          }
-          return {firstName,age,handleClick,myRef}
-      },
-  }
+            const myRef= ref(null)
+            const handleClick = () =>{
+                console.log(myRef,myRef.value)
+                myRef.value.classList.add('Dursun');
+                myRef.value.textContent='Dursun Köktürk'
+            }
+            return {firstName,age,handleClick,myRef}
+        },
+    }
 </script>
